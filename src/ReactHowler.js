02860e796d4af298f6e11fa8f0bde7b153ab201a
Slind14 <slind014@gmail.com>
2016-09-28 00:32:15 +0200

    added format to howler

--- src/ReactHowler.js
+++ src/ReactHowler.js
@@ -31,6 +31,7 @@ class ReactHowler extends Component {
     if (typeof Howl !== 'undefined') { // Check if window is available
       this.howler = new Howl({
         src: props.src,
+        format: props.format,
         autoplay: props.playing,
         mute: props.mute,
         loop: props.loop,
@@ -158,6 +159,7 @@ ReactHowler.propTypes = {
     PropTypes.string,
     PropTypes.arrayOf(PropTypes.string)
   ]).isRequired,
+  format: PropTypes.arrayOf(PropTypes.string).isRequired,
   playing: PropTypes.bool,
   mute: PropTypes.bool,
   loop: PropTypes.bool,
